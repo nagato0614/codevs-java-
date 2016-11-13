1c9a0a7f00825cceb83b0915e49eb093537fbf6a
toru_sora <nagato0614@gmail.com>
2016-11-13 09:13:49 +0900

    [update]シミュレーション用のメソッドを一通り追加

--- src/codevs/Main.java
+++ src/codevs/Main.java
@@ -1,41 +1,32 @@
 package codevs;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
+import java.awt.List;
+import java.lang.Math;
 
 public class Main {
-
+	
     public static void main(String[] args) {
         new Main().run();
     }
-
+    
     static final String AI_NAME = "allserach";
-
     static final int EMPTY = 0;
-
+    static final int SIMTIME = 3;		//simulating time
     Random random = new Random();
-
     int turn = -1;
-
     Pack[] pack;
-
     int width;
-
     int height;
-
     int packSize;
-
     int summation;
-
     int obstacle;
-
     int maxTurn;
-
     long millitime;
-
     Board my;
-
     Board op;
 
     class Board {
@@ -76,13 +67,183 @@ public class Main {
         		System.err.printf(" ]\n");
         	}
         }
+        
+        public void setPack(Pack p, int setPos) {
+        	for (int i = 0; i < packSize; i++) {
+        		for (int j = 0; j < packSize; j++) {
+        			this.simulateBoard[i][j + setPos] = p.pack[i][j];
+        		}
+        	}
+        	this.fallBlock();
+        }
+        
+        public int[] howManyChain() {
+        	int sum = 0;
+        	ArrayList<Integer> block = new ArrayList<Integer>(1);
+        	block.add(this.deleteBlock());
+        	while (block.get(sum) > 0) {
+        		sum++;
+        		this.fallBlock();
+        		block.add(this.deleteBlock());
+        	}
+        	int[] b = new int[block.size()];
+        	for (int i = 0; i < block.size(); i ++) {
+        		b[i] = block.get(i);
+        	}
+        	return b;
+        }
+        
+        public void fallBlock() {
+        	while (true) {
+        		int sum = 0;
+        		for (int i = this.simulateBoard.length - 1; i > 0; i--) {
+        			for (int j = 0; j < width; j++) {
+        				if (this.simulateBoard[i][j] == 0 && this.simulateBoard[i - 1][j] > 0) {
+        					sum++;
+        					this.simulateBoard[i][j] = this.simulateBoard[i - 1][j];
+        					this.simulateBoard[i - 1][j] = 0;
+        				}	
+        			}
+        		}
+        		if (sum == 0)
+        			break;
+        	}
+        }
+        
+        public int deleteBlock() {
+        	int deleteCount = 0;
+        	int sum = 0;
+        	boolean[] flag = new boolean[4];
+        	for (int i = this.simulateBoard.length - 1; i > 0; i--) {
+        		for (int j = 0; j < width; j++) {
+        			//when there is no block or obstacle
+        			if (this.simulateBoard[i][j] == 0 || this.simulateBoard[i][j] == obstacle) 
+        				continue;
+        			
+        			//reset flag
+        			for (int k = 0; k < flag.length; k++) 
+        				flag[k] = true;
+        			
+        			for (int x = 0; x <= summation; x++) {
+        				//horizontal
+        				if (j + (x - 1) < width && flag[0]) {
+        					sum = 0;
+        					for (int k = 0; k < x; k++) {
+        						if (this.simulateBoard[i][j + k] != 0) {
+        								sum += Math.abs(this.simulateBoard[i][j + k]);
+        						} else {
+        							flag[0] = false;
+        							break;
+        						}
+        						if (sum > summation)
+        							break;
+        					}
+        					if (flag[0] && sum == summation) {
+        						for (int k = 0; k < x; k++) {
+        							deleteCount++;
+        							this.simulateBoard[i][j + 1] = -Math.abs(this.simulateBoard[i][j + 1]);
+        						}
+        					}
+        				}
+        				
+        				//vertical
+        				if (i - (x - 1) > 0 && flag[1]) {
+        					sum = 0;
+        					for (int k = 0; k < x; k++) {
+        						if (this.simulateBoard[i - k][j] != 0) {
+        							sum += Math.abs(this.simulateBoard[i - k][j]);
+        						} else {
+        							flag[1] = false;
+        							break;
+        						}
+        						if (sum > summation)
+        							break;
+        					}
+        					if (sum == summation) {
+        						for (int k = 0; k < x; k++) {
+        							deleteCount++;
+        							this.simulateBoard[i - k][j] = -Math.abs(this.simulateBoard[i - k][j]);
+        						}
+        					}
+        				}
+        				
+        				//diagonally to the right
+        				if (flag[2] && i - (x - 1) > 0 && j + (x - 1) < width) {
+        					sum = 0;
+        					for (int k = 0; k < x; k++) {
+        						if (this.simulateBoard[i - k][j + k] != 0) {
+        							sum += Math.abs(this.simulateBoard[i - k][j + k]);
+        						} else {
+        							flag[2] = false;
+        							break;
+        						}
+        						if (sum > summation)
+        							break;
+        					}
+        					if (sum == summation) {
+        						for (int k = 0; k < x; k++) {
+        							deleteCount++;
+        							this.simulateBoard[i - k][j + k] = -Math.abs(this.simulateBoard[i - k][j + k]);
+        						}
+        					}
+        				}
+        				
+        				//diagonally to the left
+        				if (flag[3] && i - (x - 1) > 0 && j - (x - 1) >= 0) {
+        					sum = 0;
+        					for (int k = 0; k < x; k++) {
+        						if (this.simulateBoard[i - k][j - k] != 0) { 
+        							sum += Math.abs(this.simulateBoard[i - k][j - k]);
+        						} else {
+        							flag[3] = false;
+        							break;
+        						}
+        						if (sum > summation)
+        							break;
+        					}
+        					if (sum == summation) {
+        						for (int k = 0; k < x; k++) {
+        							deleteCount += 1;
+        							this.simulateBoard[i - k][j - k] = -Math.abs(this.simulateBoard[i - k][j - k]);
+        						}
+        					}
+        				}
+        			}
+        		}
+        	}
+        	for (int i = 0; i < this.simulateBoard.length; i++) {
+        		for (int j = 0; j < width; j ++) {
+        			if (this.simulateBoard[i][j] < 0) {
+        				this.simulateBoard[i][j] = EMPTY;
+        			}
+        		}
+        	}
+        	return deleteCount;
+        }
+        
+        public boolean dangerZone() {
+        	for (int i = 0; i < packSize; i++) {
+        		for (int j = 0; j < width; j++) {
+        			if (this.simulateBoard[i][j] != 0)
+        				return true;
+        		}
+        	}
+        	return false;
+        }
+        
+        public int[][] copyBoard() {
+        	int[][] res = new int[this.simulateBoard.length][];
+        	for (int i = 0; i < res.length; i++) {
+        		res[i] = Arrays.copyOf(this.simulateBoard[i], width);
+        	}
+        	return res;
+        }
     }
     
     class Pack {
     	int[][] pack = new int[width][height];
     	
     	public void packRotate(int rot) {
-    		int[][] res = this.copyPack();
     		for (int i = 0; i < rot; i++) {
     			this.rot1();
     		}
@@ -142,7 +303,6 @@ public class Main {
                 turn = in.nextInt();
                 millitime = in.nextLong();
                 my = new Board(width, height, in);
-                my.showSimulateBoard();
                 op = new Board(width, height, in);
 
                 debug("turn : " + turn);
