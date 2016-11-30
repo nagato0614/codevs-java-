d46d6f6ac63f653db6bb7ea363a96c970685c614
toru_sora <nagato0614@gmail.com>
2016-11-30 10:47:06 +0900

    [update]不必要なインポートを削除

--- src/codevs/Main.java
+++ src/codevs/Main.java
@@ -2,8 +2,6 @@ package codevs;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Random;
 import java.util.Scanner;
 import java.lang.Math;
